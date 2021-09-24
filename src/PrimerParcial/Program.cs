be5f5a710f2f1f7237d07629a93ff9907d866ea8
Andres <afgalvan@unicesar.edu.co>
2021-09-24 14:50:24 +0100

    Se confirma el punto 1

---
+++ src/PrimerParcial/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace PrimerParcial
+{
+    internal static class Program
+    {
+        private static Task Main(string[] args) =>
+            CreateHostBuilder(args).Build().RunAsync();
+
+        private static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices(InjectDependencies);
+
+        private static void InjectDependencies(IServiceCollection services)
+        {
+            // services.AddDataDependencies();
+            // services.AddLogicDependencies();
+            // services.AddPresentationDependencies();
+        }
+    }
+}
